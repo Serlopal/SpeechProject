53d575154814888739b4533f555206fdb301af0f
monicavi <monicavi@kth.se>
2017-02-28 11:01:33 +0100

    Database fix

--- load_data.m
+++ load_data.m
@@ -1,11 +1,12 @@
-function [ data,Fs ] = load_data()
-%this functions loads the audio data from the databse
+function [data, Fs] = load_data(db_path)
+% This functions loads the audio data from the database
 
+    cd(db_path);
     files = dir();
     data = cell(1,length(files));
 
     for i=1:length(files)
-        if startsWith(files(i).name,'m')
+        if endsWith(files(i).name, '.wav')
             s = strcat(files(i).folder,'\',files(i).name);
             disp(s);
             [ys,Fs] = audioread(s);
@@ -17,6 +18,6 @@ function [ data,Fs ] = load_data()
 
 
 
-
+    cd ..
 end
 
--- script.m
+++ script.m
@@ -1,6 +1,6 @@
 clear all;close all;clc;
+db = strcat(pwd, '\EnglishDB');
 
-%%This scripts loads all audio files in the  directory where the script is
-%%located into a cell structure that contains the audios and a common Fs
-%%for all of them
-[data,Fs] = load_data();
+%% This scripts loads all audio files in the directory specified above into 
+% a cell structure that contains the audios and a common Fs for all of them
+[data,Fs] = load_data(db);
