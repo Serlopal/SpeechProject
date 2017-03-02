3fe373658e7ec31d4f105a674bcde859bc1dc82a
DYusL <dyl9293@gmail.com>
2017-03-02 13:46:11 +0100

    Add function to compute formants
    Add function to compute formants

---
+++ compute_formants.m
@@ -0,0 +1,28 @@
+function [ formants ] = compute_formants( audio )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+x1 = audio.*hamming(length(x));
+
+preemph = [1 0.63];
+x1 = filter(1,preemph,x1);
+
+A = lpc(x1,8);
+rts = roots(A);
+
+rts = rts(imag(rts)>=0);
+angz = atan2(imag(rts),real(rts));
+
+[frqs,indices] = sort(angz.*(Fs/(2*pi)));
+bw = -1/2*(Fs/(2*pi))*log(abs(rts(indices)));
+
+nn = 1;
+for kk = 1:length(frqs)
+    if (frqs(kk) > 90 && bw(kk) <400)
+        formants(nn) = frqs(kk);
+        nn = nn+1;
+    end
+end
+
+end
+
