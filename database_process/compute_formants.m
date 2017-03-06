d449fc2fa8c75aeab5fcc4ea393bf748a9cb2f68
DYusL <dyl9293@gmail.com>
2017-03-06 15:17:51 +0100

    Improved clustering plotting
    Improved clustering plotting

--- database_process/compute_formants.m
+++ database_process/compute_formants.m
@@ -1,4 +1,4 @@
-function [ formants ] = compute_formants( audio, Fs )
+function [ formants ] = compute_formants( audio, Fs, poly_order )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -7,8 +7,19 @@ x1 = audio.*hamming(length(audio));
 preemph = [1 0.63];
 x1 = filter(1,preemph,x1);
 
-A = lpc(x1,18);
+A = lpc(x1,poly_order);
+
+% for men, optimal value is 15 (13 also good)
+% for women, optimal value is 13
+% for kida, optimal value is 12
+% CHECK AGAINST REAL VALUES TO FIND BEST VALUE
+
+
+% x_values = linspace(-0.5,0.5,10000);
+% plot(polyval(A,x_values), x_values);
+
 rts = roots(A);
+
 
 rts = rts(imag(rts)>=0);
 angz = atan2(imag(rts),real(rts));
--- database_process/plot_clusters.m
+++ database_process/plot_clusters.m
@@ -14,11 +14,11 @@ aw_vowels = vowels(aw_index_men,:);
 eh_index_men = strmatch('eh',indexes);
 eh_vowels = vowels(eh_index_men,:);
 
+ei_index_men = strmatch('ei',indexes);
+ei_vowels = vowels(ei_index_men,:);
+
 er_index_men = strmatch('er',indexes);
 er_vowels = vowels(er_index_men,:);
-
-ey_index_men = strmatch('ey',indexes);
-ey_vowels = vowels(ey_index_men,:);
 
 ih_index_men = strmatch('ih',indexes);
 ih_vowels = vowels(ih_index_men,:);
@@ -38,17 +38,59 @@ uh_vowels = vowels(uh_index_men,:);
 uw_index_men = strmatch('uw',indexes);
 uw_vowels = vowels(uw_index_men,:);
 
+centroid_ae = [mean(ae_vowels, 1); std(ae_vowels, 0, 1)];
+centroid_ah = [mean(ah_vowels, 1); std(ah_vowels, 0, 1)];
+centroid_aw = [mean(aw_vowels, 1); std(aw_vowels, 0, 1)];
+centroid_eh = [mean(eh_vowels, 1); std(eh_vowels, 0, 1)];
+centroid_ei = [mean(ei_vowels, 1); std(ei_vowels, 0, 1)];
+centroid_er = [mean(er_vowels, 1); std(er_vowels, 0, 1)];
+centroid_ih = [mean(ih_vowels, 1); std(ih_vowels, 0, 1)];
+centroid_iy = [mean(iy_vowels, 1); std(ah_vowels, 0, 1)];
+
+% plot(centroid_ae(1,1), centroid_ae(1,2), 'xb');
+% hold on;
+% plot_ellipse(centroid_ae(1,1), centroid_ae(1,2), centroid_ae(2,1), centroid_ae(2,2));
+% hold on;
+% plot(centroid_ah(1,1), centroid_ah(1,2), 'xr');
+% hold on;
+% plot_ellipse(centroid_ah(1,1), centroid_ah(1,2), centroid_ah(2,1), centroid_ah(2,2));
+% hold on;
+% plot(centroid_aw(1,1), centroid_aw(1,2), 'xg');
+% hold on;
+% plot_ellipse(centroid_aw(1,1), centroid_aw(1,2), centroid_aw(2,1), centroid_aw(2,2));
+% hold on;
+% plot(centroid_eh(1,1), centroid_eh(1,2), 'x');
+% hold on;
+% plot_ellipse(centroid_eh(1,1), centroid_eh(1,2), centroid_eh(2,1), centroid_eh(2,2));
+% hold on;
+% plot(centroid_ei(1,1), centroid_ei(1,2), 'xk');
+% hold on;
+% plot_ellipse(centroid_ei(1,1), centroid_ei(1,2), centroid_ei(2,1), centroid_ei(2,2));
+% hold on;
+% plot(centroid_er(1,1), centroid_er(1,2), 'xy');
+% hold on;
+% plot_ellipse(centroid_er(1,1), centroid_er(1,2), centroid_er(2,1), centroid_er(2,2));
+% hold on;
+% plot(centroid_ih(1,1), centroid_ih(1,2), 'xm');
+% hold on;
+% plot_ellipse(centroid_ih(1,1), centroid_ih(1,2), centroid_ih(2,1), centroid_ih(2,2));
+% hold on;
+% plot(centroid_iy(1,1), centroid_iy(1,2), 'xc');
+% hold on;
+% plot_ellipse(centroid_iy(1,1), centroid_iy(1,2), centroid_iy(2,1), centroid_iy(2,2));
+
+hold on;
 plot(ae_vowels(:,1),ae_vowels(:,2), 'ob' );
 hold on;
 plot(ah_vowels(:,1),ah_vowels(:,2), 'or' );
 hold on;
 plot(aw_vowels(:,1),aw_vowels(:,2), 'og' );
 hold on;
-plot(eh_vowels(:,1),eh_vowels(:,2), 'ow' );
+plot(eh_vowels(:,1),eh_vowels(:,2), 'o' );
+hold on;
+plot(ei_vowels(:,1),ei_vowels(:,2), 'ok' );
 hold on;
 plot(er_vowels(:,1),er_vowels(:,2), 'oy' );
-hold on;
-plot(ey_vowels(:,1),ey_vowels(:,2), 'ok' );
 hold on;
 plot(ih_vowels(:,1),ih_vowels(:,2), 'om' );
 hold on;
@@ -62,6 +104,23 @@ plot(uh_vowels(:,1),uh_vowels(:,2), 'o')
 hold on;
 plot(uw_vowels(:,1),uw_vowels(:,2), 'o')
 hold off;
+set(gca, 'ydir','reverse')
+set(gca, 'xdir','reverse')
+view([90 -90])
+legend('ae(had)','ah(hod)','aw(hawed)','eh(head)','ey/ei(hayed)','er(heard)','ih(hid)','iy(heed)', 'oa(hoed)', 'oo(hood)', 'uh(hud)', 'uw(who''d)')
+figure;
+
+end
+
+function [ ] = plot_ellipse(x0, y0, a, b)
+% a = horizontal radius
+% b = vertical radius
+% x0,y0 ellipse centre coordinates
+range = 1;
+t=-pi:0.01:pi;
+x=x0 + range*a*cos(t);
+y=y0 + range*b*sin(t);
+plot(x,y);
 
 end
 
